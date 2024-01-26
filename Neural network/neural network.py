6331c7dcaf94fba72fd76c10c9ad89a813c3cc68
SzFladro <68751891+SzFladro@users.noreply.github.com>
2024-01-26 09:34:25 +0000

    training accuracy peaked at 87% and val at 80%

--- Neural network/neural network.py
+++ Neural network/neural network.py
@@ -8,7 +8,7 @@ from tensorflow.keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, classification_report
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, BatchNormalization, Dropout, Reshape, Bidirectional, LSTM, Flatten, Dense
-from tensorflow.keras.callbacks import EarlyStopping, TensorBoard
+from tensorflow.keras.callbacks import EarlyStopping, TensorBoard, ModelCheckpoint
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.models import load_model, Sequential
 
@@ -27,7 +27,7 @@ def load_data(class_folder):
     landmarks = np.concatenate([landmarks,landmarks],axis=0)
     labels.extend([class_mapping[class_folder]] * 180)    
     labels = to_categorical(labels, num_classes=len(class_mapping)).astype(int)
-    X_train, X_test, y_train, y_test = train_test_split(landmarks, labels, test_size=0.4, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(landmarks, labels, test_size=0.3, random_state=42)
     X_test, X_val, y_test, y_val = train_test_split(X_test, y_test, test_size=0.5, random_state=42)
     return X_train, X_test, X_val, y_train, y_test, y_val
 
@@ -37,18 +37,16 @@ def Sequential_model(input_shape=(30, 1662)):
     # LSTM Layers
     model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=input_shape))
     model.add(LSTM(128, return_sequences=True, activation='relu'))
-    ##return sequences is false, prevents the LTSM layer from returning sequences to the next Dense layer
+    ##return sequences is false, prevents the LTSM layer from returning sequences to the next Dnese layer
     model.add(LSTM(256, return_sequences=False, activation='relu'))
 
-    model.add(BatchNormalization())
+    # Flatten Layer
+    model.add(Flatten())
 
     # Dense Layers with Dropout
     model.add(Dense(256, activation='relu'))
-    model.add(BatchNormalization())
     model.add(Dense(128, activation='relu'))
-    model.add(BatchNormalization())
     model.add(Dense(64, activation='relu'))
-    model.add(BatchNormalization())
 
     # Output Layer
     model.add(Dense(len(class_mapping), activation='softmax'))
@@ -62,13 +60,14 @@ def visualise(model):
     ##using mini-batch gradient descent to train the neural network (using a batch size that is >1 but < training set)
 def train_model(model, X_train, y_train, X_val, y_val, epochs=1000, batch_size = 64):
     log_dir = os.path.join(os.getcwd(),'Neural network','Logger')
-    tb_callback = TensorBoard(log_dir=log_dir)
 
+    tb_callback = TensorBoard(log_dir=log_dir)
+    early_stopping = EarlyStopping(monitor='categorical_accuracy', patience = 100, restore_best_weights=True)
     history = model.fit(X_train, y_train, 
                         batch_size=batch_size,
                         epochs=epochs, 
                         shuffle=True, 
-                        callbacks=[tb_callback], 
+                        callbacks=[early_stopping,tb_callback], 
                         validation_data =(X_val, y_val))
     model.save('BSLmodel.h5')
     model.save_weights('BSLweights.h5')
@@ -86,6 +85,7 @@ if __name__ == "__main__":
     # Compile the model
     model = Sequential_model()
     model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
+
     # load data from every class (letters of the alphabet) and split them into training and testing data
     for class_name in class_mapping:
         data_train, data_test, data_val, label_train, label_test, label_val = load_data(class_name)
