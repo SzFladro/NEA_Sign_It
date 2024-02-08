d562edd9ba5590adb850fd385fc61d5cc647a728
SzFladro <68751891+SzFladro@users.noreply.github.com>
2024-02-08 11:26:32 +0000

    added more comments

--- Neural network/Data collection.py
+++ Neural network/Data collection.py
@@ -21,7 +21,6 @@ class VideoDataPreprocessor:
         self.class_mapping = {}
         self.args = self.get_args()
 
-    ##method that stores 
     def get_args(self):
         parser = argparse.ArgumentParser()
         parser.add_argument("--shape", help='shape of array', type=int, default=1662)
@@ -31,53 +30,49 @@ class VideoDataPreprocessor:
                             default=0.7)
         parser.add_argument("--min_tracking_confidence", help='min_tracking_confidence', type=float,
                             default=0.5)
- 
-
         args = parser.parse_args()
         return args
 
-    ##method for 
+    ##method for processing frames and locating the hands,face,pose from the image
     def mediapipe_detection(self,image, model):
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # COLOR CONVERSION BGR 2 RGB
-        image.flags.writeable = False                  # Image is no longer writeable
+        image.flags.writeable = False                  # locks the image so that it isnt writable
         results = model.process(image)                 # Make prediction
-        image.flags.writeable = True                   # Image is now writeable 
+        image.flags.writeable = True                   # unlocks the image to allow for connections to be drawn on 
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) # COLOR COVERSION RGB 2 BGR
         return image, results
 
-    def draw_styled_landmarks(self,image, results,mp_holistic ):
-        mp_drawing = mp.solutions.drawing_utils # Drawing utilities
+    ##method for drawing the different connections to the frame using the coordinates of the different parts collected (landmarks)
+    def draw_styled_landmarks(self,frame, results,holisticModel ):
+        mp_drawer = mp.solutions.drawing_utils # Drawing utilities
 
         # Draw face connections
-        mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION, 
-                                 mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1), 
-                                 mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1)
-                                 ) 
+        mp_drawing.draw_landmarks(frame, results.face_landmarks, holisticModel.FACEMESH_TESSELATION, 
+                                 mp_drawer.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1), 
+                                 mp_drawer.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1)) 
         # Draw pose connections
-        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
-                                 mp_drawing.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4), 
-                                 mp_drawing.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=2)
-                                 ) 
+        mp_drawing.draw_landmarks(frame, results.pose_landmarks, holisticModel.POSE_CONNECTIONS,
+                                 mp_drawer.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4), 
+                                 mp_drawer.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=2)) 
         # Draw left hand connections
-        mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
-                                 mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4), 
-                                 mp_drawing.DrawingSpec(color=(121,44,250), thickness=2, circle_radius=2)
-                                 ) 
+        mp_drawing.draw_landmarks(frame, results.left_hand_landmarks, holisticModel.HAND_CONNECTIONS, 
+                                 mp_drawer.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4), 
+                                 mp_drawer.DrawingSpec(color=(121,44,250), thickness=2, circle_radius=2)) 
         # Draw right hand connections  
-        mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
-                                 mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4), 
-                                 mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
-                             ) 
-    ##method for 
+        mp_drawing.draw_landmarks(frame, results.right_hand_landmarks, holisticModel.HAND_CONNECTIONS, 
+                                 mp_drawer.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4), 
+                                 mp_drawer.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)) 
+        
+    ##method for collecting seperating the different coordinates into their respective groups and storing it as an array
     def extract_keypoints(self,results):
-        pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33*4)
-        face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(468*3)
-        lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
-        rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
-        return np.concatenate([pose, face, lh, rh])
+        pose = np.array([[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33*4)
+        face = np.array([[landmark.x, landmark.y, landmark.z] for landmark in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(468*3)
+        lefthand = np.array([[landmark.x, landmark.y, landmark.z] for landmark in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
+        righthand = np.array([[landmark.x, landmark.y, landmark.z] for landmark in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
+        return np.concatenate([pose, face, lefthand, righthand])
 
     def preprocess_data(self,class_list):
-        mp_holistic = mp.solutions.holistic  # Holistic model
+        holisticModel = mp.solutions.holistic  
         cap = cv2.VideoCapture(0)
         args = self.get_args()
         detection_confidence = args.min_detection_confidence
@@ -86,16 +81,14 @@ class VideoDataPreprocessor:
         NoFrames = args.frames
         Shape = args.shape
 
-        with mp_holistic.Holistic(min_detection_confidence=detection_confidence, min_tracking_confidence= tracking_confidence) as holistic:
+        with holisticModel.Holistic(min_detection_confidence=detection_confidence, min_tracking_confidence= tracking_confidence) as holistic:
             while cap.isOpened():
-
                 for class_name in class_list:
                     class_data = np.empty((0, NoFrames, Shape), dtype=np.float32)
                     Vidlandmarks = np.empty((NoVideos, NoFrames, Shape), dtype=np.float32)
 
                     for VidNo in range(NoVideos):
                         for frame_num in range(NoFrames):
-
                             ret, frame = cap.read()
 
                             # Make detections
@@ -104,20 +97,19 @@ class VideoDataPreprocessor:
                             # Draw landmarks
                             self.draw_styled_landmarks(image, results,mp_holistic )
                 
-                            # NEW Apply wait logic
                             if frame_num == 0: 
-                                cv2.putText(image, 'STARTING COLLECTION', (120,200), 
+                                cv2.putText(image, 'Collecting first Frame', (120,200), 
                                            cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255, 0), 4, cv2.LINE_AA)
-                                cv2.putText(image, 'Collecting frames for {} Video Number {}'.format(class_name, VidNo), (15,12), 
+                                cv2.putText(image, 'Collecting class {} frames, Video {}'.format(class_name, VidNo), (15,12), 
                                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
                                 # Show to screen
-                                cv2.imshow('OpenCV Feed', image)
+                                cv2.imshow('Data Collection', image)
                                 cv2.waitKey(500)
                             else: 
-                                cv2.putText(image, 'Collecting frames for {} Video Number {}'.format(class_name, VidNo), (15,12), 
+                                cv2.putText(image, 'Collecting class {} frames Video {}'.format(class_name, VidNo), (15,12), 
                                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
                                 # Show to screen
-                                cv2.imshow('OpenCV Feed', image)
+                                cv2.imshow('Data Collection', image)
                 
                             keypoints = self.extract_keypoints(results)
                             print(keypoints)
@@ -139,9 +131,8 @@ class VideoDataPreprocessor:
         self.class_mapping = {class_name: i for i, class_name in enumerate(class_list)}
     
     '''
-    method for saving the one hot encoded label 
+    method for saving the one hot encoded label along with the corresponding data into numpy files
     '''
-
     def save_class_data(self, class_name, class_data):
         numerical_label = self.class_mapping[class_name]
         one_hot_label = np.zeros(len(self.class_mapping), dtype=int)
